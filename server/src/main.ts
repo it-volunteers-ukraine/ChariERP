ac1989520468f50d7c7269036d3307caff63ecdd
Andrey Teslenko <andteslenko@gmail.com>
2024-03-03 20:25:12 +0200

    added  swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+
+const port = 4200;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('CheriERP API')
+    .setDescription('Rest API Documentation')
+    .setVersion('1.0')
+    .addTag('CheriERP')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(port || 4200, () =>
+    console.log(`Application is running on the port: ${port}`),
+  );
 }
+
 bootstrap();
