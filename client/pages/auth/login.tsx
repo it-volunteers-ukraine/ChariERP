bf7223ee31a0a6437d085405a605248319478459
Andrey Teslenko <andteslenko@gmail.com>
2024-04-21 16:50:23 +0300

    created structure login page

---
+++ client/pages/auth/login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div>Loging</div>;
+};
+
+export default Login;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,36 +1,10 @@
-import { Header, Input } from '@/components';
+import { Header } from '@/components';
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <div className="flex w-full h-full flex-col items-center justify-center p-10">
-        <Input
-          label="Довідка про внесення в реєстр"
-          error="1"
-          placeholder="Placeholder"
-          placeholderItalic
-        />
-        <Input label="Login" error="" placeholder="Placeholder" />
-        <Input
-          label="Login"
-          error=""
-          placeholder="Placeholder"
-          type="password"
-        />
-        <Input
-          disabled
-          label="Login"
-          placeholder="Placeholder"
-          info={
-            <>
-              Українською мовою (відповідно до Статуту) Наприклад: Громадська
-              організація{' '}
-              <span className="font-bold text-input-focus">«ЖИВИ»</span>
-            </>
-          }
-        />
-      </div>
+      <div className="flex w-full h-full flex-col items-center justify-center p-10"></div>
     </div>
   );
 }
