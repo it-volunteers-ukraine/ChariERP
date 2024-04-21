0eab3860d997a9c65c04025b84a3d8e22c358960
Serhii Cheremisin <s.cheremisin1991@gmail.com>
2024-04-21 17:09:12 +0300

    Refactored code

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,8 @@
-import { Header, Input } from '@/components';
+import { Input } from '@/components';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <div className="flex w-full h-full flex-col items-center justify-center p-10">
         <Input label="Логин" error="1" />
         <Input label="Login" error="" />
