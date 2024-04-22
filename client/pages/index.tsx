b07ac1f1a1d09af580b09b1c105834a864502d1d
ASGladchenko <asgladchenko@gmail.com>
2024-04-22 12:00:51 +0300

    delete dashboard from main page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,4 @@
-import { DashboardAside, DashboardHeader, Header, Input } from '@/components';
+import { Header, Input } from '@/components';
 
 export default function Home() {
   return (
@@ -7,14 +7,6 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center w-full h-full p-10">
         <Input label="Логин" error="1" />
         <Input label="Login" error="" />
-      </div>
-
-      <div className="flex h-dvh">
-        <DashboardAside />
-        <div className="flex flex-col w-[calc(100vw-290px)]">
-          <DashboardHeader />
-          <div className="w-full h-full overflow-y-auto"></div>
-        </div>
       </div>
     </>
   );
