f28c7042c0499efb7da3adf963d074890ff8fdf8
ASGladchenko <asgladchenko@gmail.com>
2024-04-23 09:56:35 +0300

    Change img to Next Image

--- client/components/dashboard/avatar/index.tsx
+++ client/components/dashboard/avatar/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { Icon } from '@/assets';
 
 import { IAvatarProps } from './types';
@@ -8,7 +10,7 @@ export const Avatar = ({ img, name }: IAvatarProps) => {
   return (
     <div className="flex gap-2 transition-all duration-200 cursor-pointer text-lightBlue hover:drop-shadow-xl">
       {img !== null ? (
-        <img />
+        <Image src={img} alt={name} className="w-24 h-24 rounded-full" />
       ) : (
         <Icon.DashboardIcon.AvatarIcon width={24} height={24} />
       )}
