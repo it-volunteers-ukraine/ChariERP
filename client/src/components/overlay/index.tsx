08c7d95875c721530a904f93e942248eeda52ecc
Andrey Teslenko <andteslenko@gmail.com>
2024-06-12 14:05:41 +0300

    added tab trap

--- client/src/components/overlay/index.tsx
+++ client/src/components/overlay/index.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { useEffect, useRef } from 'react';
+
 import { useMounted } from '@/hooks';
 import { Close } from '@/assets/icons';
 import { ChildrenProps } from '@/types';
@@ -17,9 +20,56 @@ export const Overlay = ({
   children,
   duration = 300,
 }: ChildrenProps<IOverlayProps>) => {
+  const modalRef = useRef<HTMLDivElement>(null);
   const { mounted } = useMounted({ opened, duration });
 
   const styles = getStyles(opened);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+
+      if (event.key === 'Tab' && modalRef.current) {
+        const focusableElements =
+          modalRef.current.querySelectorAll<HTMLElement>(
+            'a[href], button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])',
+          );
+        const firstElement = focusableElements[0];
+        const lastElement = focusableElements[focusableElements.length - 1];
+
+        if (event.shiftKey) {
+          if (document.activeElement === firstElement) {
+            event.preventDefault();
+            lastElement.focus();
+          }
+        } else {
+          if (document.activeElement === lastElement) {
+            event.preventDefault();
+            firstElement.focus();
+          }
+        }
+      }
+    };
+
+    if (opened) {
+      window.addEventListener('keydown', handleKeyDown);
+    } else {
+      window.removeEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [opened, onClose]);
+
+  const handleKeyPressOnClose = (event: React.KeyboardEvent<SVGSVGElement>) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      onClose();
+    }
+  };
 
   if (!mounted) return null;
 
@@ -32,14 +82,18 @@ export const Overlay = ({
           style={{ animationDuration: `${duration}ms` }}
         />
         <div
+          ref={modalRef}
           className={styles.modal}
           style={{ animationDuration: `${duration - 20}ms` }}
         >
           <Close
             width={24}
             height={24}
+            tabIndex={0}
+            role="button"
             onClick={onClose}
             className={styles.svg}
+            onKeyPress={handleKeyPressOnClose}
           />
           {children}
         </div>
