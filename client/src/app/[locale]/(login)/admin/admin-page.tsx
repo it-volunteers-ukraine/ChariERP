f0fbe642e8566e93b4cf7597556a0907974e5ca1
Andrey Teslenko <andteslenko@gmail.com>
2024-07-02 22:30:16 +0300

    Added error handling

--- client/src/app/[locale]/(login)/admin/admin-page.tsx
+++ client/src/app/[locale]/(login)/admin/admin-page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { FormikValues } from 'formik';
 import axios, { AxiosError } from 'axios';
+import { FormikHelpers, FormikValues } from 'formik';
 import { useRouter } from 'next/navigation';
 
 import { routes } from '@/constants';
@@ -10,14 +10,14 @@ import { LoginForm } from '../login-form';
 const AdminPage = () => {
   const router = useRouter();
 
-  const onSubmit = async ({ email, password }: FormikValues) => {
+  const onSubmit = async ({ email, password }: FormikValues, handleFormik?: FormikHelpers<FormikValues>) => {
     try {
       await axios.post(`/api/admin`, { email, password }).then(({ data }) => data);
 
       router.push(routes.dashboard);
     } catch (error: unknown) {
       if (error instanceof AxiosError) {
-        console.log(error.response?.data);
+        handleFormik?.setFieldError('email', error.response?.data.message);
       }
     }
   };
