db0f370ac9d80371732c03748cdf7d34dfcb58c4
ASGladchenko <105244016+ASGladchenko@users.noreply.github.com>
2024-07-27 11:53:34 +0300

    create and connect 404 page (#113)

---
+++ client/src/app/[locale]/[...not-found]/style.ts
@@ -0,0 +1,9 @@
+export const getStyles = () => ({
+  wrapper:
+    'w-screen h-screen flex flex-col justify-center items-center bg-white gap-10 desktop:flex-row desktop:gap-10 desktopXl:gap-[80px]',
+  text: 'font-scada text-2xl font-bold leading-[28.8px] text-darkGray uppercase tablet:text-[32px] tablet:leading-[38.4px] desktop:text-[40px] desktop:leading-[40px] desktopXl:text-[48px] desktopXl:leading-[57.6px]',
+  error:
+    'text-[160px] text-arcticSky leading-[100%] font-bold font-scada tablet:text-[320px] tablet:leading-[384px] desktop:text-[460px] desktop:leading-[552px] desktop:order-1 desktopXl:text-[500px] desktopXl:leading-[600px]',
+  lastText: 'mb-5 desktop:mb-10',
+  textWrapper: 'flex flex-col items-center desktop:max-w-[360px] desktop:items-start',
+});
