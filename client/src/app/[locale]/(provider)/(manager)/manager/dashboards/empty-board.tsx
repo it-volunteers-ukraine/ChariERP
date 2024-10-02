a1cdeb38806eb90fbf6c4d7fd5f2150a71ce27cd
JoeylMS <ivbazulyk@gmail.com>
2024-10-02 20:27:13 +0300

    added new icon, cleaner files

--- client/src/app/[locale]/(provider)/(manager)/manager/dashboards/empty-board.tsx
+++ client/src/app/[locale]/(provider)/(manager)/manager/dashboards/empty-board.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import React from 'react';
 import { useTranslations } from 'use-intl';
 
-import { Info } from '@/assets/icons';
+import { InfoForBoard } from '@/assets/icons';
 
 export const EmptyBoard = () => {
   const memberText = useTranslations('board');
@@ -12,7 +11,7 @@ export const EmptyBoard = () => {
     <div className="w-full h-[calc(100dvh-64px)] desktop:h-[calc(100dvh-96px)] pb-16 flex justify-center items-center">
       <div className="max-w-[343px] tablet:max-w-[598px] laptop:max-w-[706px]">
         <div className="text-lightBlue max-w-[140px] mb-8 mx-auto p-[18px]">
-          <Info />
+          <InfoForBoard />
         </div>
 
         <h3 className="font-roboto text-2xl leading-7 tablet:text-[32px] tablet:leading-9 laptop:text-[40px] laptop:leading-[47px] text-center text-lynch opacity-50 p-8 select-none">
