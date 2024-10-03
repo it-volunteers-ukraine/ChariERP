c9fd571a670303b55baa0ac139d810f939eed069
Ihor Bazulyk <105243926+JoeYlMS@users.noreply.github.com>
2024-10-03 05:17:23 +0300

    232 display page when employee has no board cards (#235)
    * create EmptyBoard
    
    * fixed EmptyBoard &  added  useTranslations
    
    * added new icon, cleaner files

--- client/src/app/[locale]/(provider)/(manager)/manager/dashboards/page.tsx
+++ client/src/app/[locale]/(provider)/(manager)/manager/dashboards/page.tsx
@@ -1,12 +1,18 @@
 import { Metadata } from 'next';
 
+import { EmptyBoard } from './empty-board';
+
 export const metadata: Metadata = {
   title: 'Dashboards',
   description: 'Dashboards page',
 };
 
 const Dashboards = () => {
-  return <>Dashboards</>;
+  return (
+    <>
+      <EmptyBoard />
+    </>
+  );
 };
 
 export default Dashboards;
