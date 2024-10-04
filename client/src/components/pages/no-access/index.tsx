cc3e571ce973ee060f055ca755874ef0b748b015
Denys Kyslytsia <kislica779@gmail.com>
2024-10-04 20:57:13 +0300

    210 changed i18 and fixed warning with scroll

--- client/src/components/pages/no-access/index.tsx
+++ client/src/components/pages/no-access/index.tsx
@@ -16,21 +16,23 @@ export const NoAccess = () => {
 
   return (
     <>
-      <div className="fixed h-full w-full blur-sm bg-white overflow-hidden">
-        <Image src={NoAccessBG} alt="bg" objectFit="none" className="min-w-[1630px]" />
-      </div>
+      <div className="relative h-full w-full bg-white overflow-hidden">
+        <div className="fixed blur-[7px]">
+          <Image src={NoAccessBG} priority={true} alt="bg" className="min-w-[1630px]" />
+        </div>
 
-      <div className="absolute inset-0 flex items-center justify-center">
-        <div className="flex flex-col gap-8 p-[32px_20px] mx-[16px] w-full max-w-[416px] bg-white drop-shadow-modal rounded-[8px]">
-          <Lock width={48} height={48} className="m-auto" />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="flex flex-col gap-8 p-[32px_20px] mx-[16px] w-full max-w-[416px] bg-white drop-shadow-modal rounded-[8px]">
+            <Lock width={48} height={48} className="m-auto" />
 
-          <span className="text-center">{text('modalUserWithoutAccess.text')}</span>
+            <span className="text-center">{text('modalUserWithoutAccess.text')}</span>
 
-          <Button
-            text={btn('goToAllBoards')}
-            className="w-[221px] uppercase m-auto"
-            onClick={() => router.push(routes.managerDashboards)}
-          />
+            <Button
+              text={btn('goToAllBoards')}
+              className="w-[221px] uppercase m-auto"
+              onClick={() => router.push(routes.managerDashboards)}
+            />
+          </div>
         </div>
       </div>
     </>
