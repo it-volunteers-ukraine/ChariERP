84abed8ad14c6a1da8800601905d4edea9e04624
Denys Kyslytsia <kislica779@gmail.com>
2024-10-06 15:48:58 +0300

    fixed email validation

--- client/src/components/formik-config/validation/enterEmailValidation.ts
+++ client/src/components/formik-config/validation/enterEmailValidation.ts
@@ -1,12 +1,14 @@
 import * as Yup from 'yup';
 import { TranslationValues } from 'next-intl';
 
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 export const enterEmailValidation = (error: (key: string, params?: TranslationValues) => string) =>
   Yup.object().shape({
     email: Yup.string()
       .trim()
       .min(6, error('minPlural', { int: 6 }))
       .max(50, error('maxPlural', { int: 50 }))
-      .email(error('notValidEmail'))
+      .matches(emailRegex, error('notValidEmail'))
       .required(error('required')),
   });
