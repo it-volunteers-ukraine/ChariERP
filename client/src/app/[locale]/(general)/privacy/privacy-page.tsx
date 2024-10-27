e900b281eb4f52cd248e31bab83c22c4c33ac90f
Davydiuk Dmytro <davudyuck0501@gmail.com>
2024-10-27 13:32:50 +0200

    create policy page

---
+++ client/src/app/[locale]/(general)/privacy/privacy-page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Title } from '@/components';
+
+const PrivacyPage = () => {
+  return (
+    <section className="h-screen bg-bgAuthGradient text-center">
+      <Title title="awdwa" className="font-scada text-[32px] font-bold uppercase leading-10" />
+    </section>
+  );
+};
+
+export { PrivacyPage };
