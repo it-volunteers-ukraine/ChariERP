2e7b4069c0e31effe2680c699c794e6baef6e3f5
Davydiuk Dmytro <davudyuck0501@gmail.com>
2024-11-05 14:55:08 +0200

    added a router to the privacy policy page

--- client/src/app/[locale]/(general)/layout.tsx
+++ client/src/app/[locale]/(general)/layout.tsx
@@ -3,10 +3,10 @@ import { Footer, Header } from '@/components';
 
 export default async function Layout({ children }: ChildrenProps) {
   return (
-    <>
+    <div className="scroll-blue flex h-screen flex-col overflow-y-auto">
       <Header />
-      <main>{children}</main>
+      <main className="flex-grow">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
