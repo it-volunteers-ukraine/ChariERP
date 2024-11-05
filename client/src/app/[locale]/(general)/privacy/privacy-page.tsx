f9f062ad44bc90ce01a2a92c995062d394dcce96
Davydiuk Dmytro <davudyuck0501@gmail.com>
2024-11-05 14:06:42 +0200

    Added a page on policies and styles

--- client/src/app/[locale]/(general)/privacy/privacy-page.tsx
+++ client/src/app/[locale]/(general)/privacy/privacy-page.tsx
@@ -1,11 +1,35 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
+
 import { Title } from '@/components';
 
+import { getStyles } from './styles';
+
 const PrivacyPage = () => {
+  const styles = getStyles();
+
+  const text = useTranslations('privacyPage');
+
   return (
-    <section className="h-screen bg-bgAuthGradient text-center">
-      <Title title="awdwa" className="font-scada text-[32px] font-bold uppercase leading-10" />
+    <section className={styles.wrapper}>
+      <Title title={text('title')} className={styles.title} />
+      <p className={styles.subTitle}>{text('subTitle')}</p>
+      <p className={styles.text}>{text('introText')}</p>
+      <p
+        className={styles.text}
+        dangerouslySetInnerHTML={{
+          __html: text('list'),
+        }}
+      />
+      <p
+        className={styles.text}
+        dangerouslySetInnerHTML={{
+          __html: text('informationDetails', {
+            link: `<a href="https://www.facebook.com/it.volunteers.ukraine" target="_blank" class="${styles.link}">facebook.com/it.volunteers.ukraine</a>`,
+          }),
+        }}
+      />
     </section>
   );
 };
