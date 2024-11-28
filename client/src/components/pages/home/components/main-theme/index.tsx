be1e4a8261e696f11b3dba7f41dea63c05054cd3
Nox <heze362003@gmail.com>
2024-11-28 10:32:36 +0200

    348 fix home page

--- client/src/components/pages/home/components/main-theme/index.tsx
+++ client/src/components/pages/home/components/main-theme/index.tsx
@@ -12,7 +12,7 @@ export const MainTheme = () => {
         alt="bg"
         priority={true}
         src={MainThemeBg}
-        className="laptop::min-h-[416px] min-h-[248px] min-w-[490px] object-cover tablet:min-h-[323px] tablet:min-w-full desktop:min-h-[604px] desktopXl:min-h-[740px]"
+        className="laptop::min-h-[416px] min-h-[248px] min-w-[490px] object-cover tablet:min-h-[323px] tablet:min-w-full desktop:min-h-[calc(100vh-76px)]"
       />
 
       <div className="absolute inset-0 flex h-full items-center justify-center">
