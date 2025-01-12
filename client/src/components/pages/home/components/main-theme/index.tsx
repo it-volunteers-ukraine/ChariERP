a7e30c8e22338c58a89de1fa2ffceb190175931d
Denys <99205779+Kyslytsia@users.noreply.github.com>
2025-01-12 12:22:06 +0200

    383 fixed (#408)

--- client/src/components/pages/home/components/main-theme/index.tsx
+++ client/src/components/pages/home/components/main-theme/index.tsx
@@ -12,7 +12,7 @@ export const MainTheme = () => {
         alt="bg"
         priority={true}
         src={MainThemeBg}
-        className="laptop::min-h-[416px] min-h-[248px] min-w-[490px] object-cover tablet:min-h-[323px] tablet:min-w-full desktop:min-h-[calc(100vh-76px)]"
+        className="laptop::min-h-[416px] min-h-[248px] min-w-[490px] object-cover tablet:min-h-[323px] tablet:min-w-full desktop:min-h-[calc(100vh-64px)]"
       />
 
       <div className="absolute inset-0 flex h-full items-center justify-center">
