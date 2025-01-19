8f276f340fc62398ad25fc8c9459e7fe65f81850
Vlad <vladkostirya@gmail.com>
2025-01-19 19:22:28 +0200

    fix style

--- client/src/components/date-field-with-title/index.tsx
+++ client/src/components/date-field-with-title/index.tsx
@@ -6,8 +6,8 @@ interface IDataFieldWithTitle {
 
 export const DateFieldWithTitle = ({ title, children }: ChildrenProps<IDataFieldWithTitle>) => {
   return (
-    <div className="flex w-full flex-col justify-between gap-2 tablet:w-full desktop:w-[451px] desktop:flex-row desktop:gap-6">
-      <h4 className="min-w-[130px] font-roboto font-medium text-comet">{title}</h4>
+    <div className="flex w-full flex-col justify-between gap-2 tablet:w-full desktop:flex-row desktop:gap-6">
+      <h4 className="min-w-max font-roboto font-medium text-comet">{title}</h4>
       {children}
     </div>
   );
