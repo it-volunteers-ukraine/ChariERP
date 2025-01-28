5e27334bb99f4fa45fd9653103336d66b6675a37
Denys Kyslytsia <kislica779@gmail.com>
2025-01-28 19:54:22 +0200

    Added hover and active for AddCard

--- client/src/components/pages/home/index.tsx
+++ client/src/components/pages/home/index.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import { Attachments } from '../../attachments';
 import { ForNGOs, KeyAdvantages, MainTheme, OpportunityChariCircle, RevealOnScroll } from './components';
 
 export const HomePage = () => {
   return (
     <div className="bg-bgAuthGradient">
+      <Attachments />
+
       <MainTheme />
 
       <div className="m-auto flex max-w-[1920px] flex-col gap-[96px] pb-[96px] tablet:gap-[120px] tablet:pb-[100px] laptop:gap-[150px] laptop:pb-[150px]">
