a0fab182cb45c57128353efe87009cbc16f9c4f0
Denys Kyslytsia <kislica779@gmail.com>
2025-02-04 20:06:55 +0200

    Added filesFormat in global constants

--- client/src/components/attachments/add-card/index.tsx
+++ client/src/components/attachments/add-card/index.tsx
@@ -1,12 +1,12 @@
 import { useTranslations } from 'next-intl';
 
 import { Plus } from '@/assets/icons';
+import { filesFormat } from '@/constants';
 
 import { Wrapper } from '../wrapper';
 
 export const AddCard = ({ addFile }: { addFile: (event: React.ChangeEvent<HTMLInputElement>) => void }) => {
   const text = useTranslations('attachments');
-  const formatFiles = '.jpg,.jpeg,.png,.svg,.webp,.xls,.xlsx,.txt,.docx,.doc,.csv,.pdf,.mp3,.wav,.mp4,.rar,.zip';
 
   return (
     <Wrapper>
@@ -15,7 +15,7 @@ export const AddCard = ({ addFile }: { addFile: (event: React.ChangeEvent<HTMLIn
 
         <span className="text-lynch/50">{text('attachFile')}</span>
 
-        <input multiple type="file" className="hidden" onChange={addFile} accept={formatFiles} />
+        <input multiple type="file" className="hidden" onChange={addFile} accept={filesFormat} />
       </label>
     </Wrapper>
   );
