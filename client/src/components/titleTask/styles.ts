000cacc31280a7288289a692c22d0217b355c871
Davydiuk Dmytro <davudyuck0501@gmail.com>
2025-05-07 16:30:13 +0300

    fix styles

--- client/src/components/titleTask/styles.ts
+++ client/src/components/titleTask/styles.ts
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 
 export const getStyles = (isEdit: boolean) => ({
-  wrapper: 'w-full max-w-[777px] desktop:max-w-[935px]',
+  wrapper: 'w-full max-w-[777px] desktop:max-w-[935px] min-h-[40px]',
   title: clsx(
     'w-full max-h-[86px] cursor-text scroll-blue outline-none font-scada font-bold text-[26px] text-lightBlue uppercase leading-[28px] resize-none bg-transparent focus:outline-none',
     !isEdit && 'line-clamp-3 break-words',
