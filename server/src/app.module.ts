22fbeef876d26cc20a08f1caf96f9b1f5047fdbe
Roman Kyrychenko <rp.kyrychenko@gmail.com>
2025-05-20 20:40:06 +0300

    Refactor and enhance server API, add Swagger integration
    Reorganized folder structures for controller and service, introduced Swagger for API documentation with CORS support, and updated dependencies for improved stability and functionality.
    Removed unnecessary legacy AWS task definition and refined e2e tests for better accuracy.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './api/app.controller';
+import { AppService } from './service/app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      envFilePath: ['.env', '.env.production.local', '.env.development.local'],
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_URI,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Chari ERP server API' as string)
+    .setDescription('General API of the back-end part of the project')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 8080);
 }
-bootstrap();
+void bootstrap();
