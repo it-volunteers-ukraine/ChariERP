807d5d114f48e2b471dce3d1256e255bbeb1e2f9
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2025-06-29 11:42:19 +0300

    refactor(feedback): remove MongoDB persistence, send email only

--- server/src/feedback/dto/feedback-response.dto.ts
+++ server/src/feedback/dto/feedback-response.dto.ts
@@ -5,15 +5,4 @@ export class FeedbackResponseDto {
     example: 'Thanks for your feedback',
   })
   message: string;
-
-  @ApiProperty({
-    example: '665fa8f0234a22f5c3fa7cd1',
-  })
-  id: string;
-
-  @ApiProperty({
-    example: '2025-06-24T12:34:56.789Z',
-    format: 'date-time',
-  })
-  createdAt: Date;
 }
--- server/src/feedback/feedback.module.ts
+++ server/src/feedback/feedback.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { FeedbackService } from './feedback.service';
 import { FeedbackController } from './feedback.controller';
-import { Feedback, FeedbackSchema } from './schemas/feedback.schema';
 import { EmailModule } from '../email/email.module';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: Feedback.name, schema: FeedbackSchema },
-    ]),
-    EmailModule,
-  ],
+  imports: [EmailModule],
   controllers: [FeedbackController],
   providers: [FeedbackService],
 })
