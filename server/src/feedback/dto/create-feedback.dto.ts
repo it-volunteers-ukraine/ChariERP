8a81ddd692497eecc22a72667dc77a7a7353be75
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2025-07-03 12:04:30 +0300

    feat(dto): add transform decorator for create feedback dto

--- server/src/feedback/dto/create-feedback.dto.ts
+++ server/src/feedback/dto/create-feedback.dto.ts
@@ -7,6 +7,7 @@ import {
   Matches,
   MaxLength,
 } from 'class-validator';
+import { Transform } from 'class-transformer';
 import { VALIDATION_MESSAGES } from '../../constants/validation-messages';
 import { UA_PHONE_REGEX } from '../../constants/regex.constants';
 
@@ -14,21 +15,25 @@ export class CreateFeedbackDto implements IFeedback {
   @ApiProperty({ required: true, example: 'Doe' })
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }: { value: string }) => value.trim())
   lastname: string;
 
   @ApiProperty({ required: true, example: 'John' })
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }: { value: string }) => value.trim())
   firstname: string;
 
   @ApiProperty({ required: true, example: 'john.doe@example.com' })
   @IsNotEmpty()
   @IsEmail({}, { message: VALIDATION_MESSAGES.EMAIL.INVALID })
+  @Transform(({ value }: { value: string }) => value.trim().toLowerCase())
   email: string;
 
   @ApiProperty({ required: true, example: '+380991234567' })
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }: { value: string }) => value.trim())
   @Matches(UA_PHONE_REGEX, {
     message: VALIDATION_MESSAGES.PHONE.INVALID,
   })
@@ -37,6 +42,7 @@ export class CreateFeedbackDto implements IFeedback {
   @ApiProperty({ required: true, example: 'Thank you for the great service!' })
   @IsNotEmpty()
   @IsString()
+  @Transform(({ value }: { value: string }) => value.trim())
   @MaxLength(400, {
     message: VALIDATION_MESSAGES.FEEDBACK.MESSAGE_TOO_LONG,
   })
