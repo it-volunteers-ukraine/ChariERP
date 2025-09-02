00ae2b33806f601540b51498d336fac17fc7e87e
Maryna Aksakova <121823496+Marixa82@users.noreply.github.com>
2025-09-02 11:47:36 +0300

    improving readability of tests

--- server/src/auth/auth.guard.spec.ts
+++ server/src/auth/auth.guard.spec.ts
@@ -7,8 +7,9 @@ describe('AuthGuard', () => {
   let mockJwtService: { verifyAsync: jest.Mock };
   let mockReflector: { getAllAndOverride: jest.Mock };
 
-  const VALID_TOKEN = 'valid';
+  const VALID_TOKEN = faker.internet.jwt({ header: { alg: 'HS256' } });
   const INVALID_TOKEN = 'invalid';
+  const MOCK_USER = { id: faker.database.mongodbObjectId() };
 
   function makeContext(
     headers: Record<string, string> = {},
@@ -24,9 +25,7 @@ describe('AuthGuard', () => {
       ...overrides,
     } as unknown as ExecutionContext;
   }
-  function expectJwtNotCalled() {
-    expect(mockJwtService.verifyAsync).not.toHaveBeenCalled();
-  }
+
   function makeAuthContext(token?: string) {
     return makeContext(token ? { authorization: `Bearer ${token}` } : {});
   }
@@ -37,49 +36,59 @@ describe('AuthGuard', () => {
     guard = new AuthGuard(mockJwtService as any, mockReflector as any);
   });
 
-  it('should allow access if route is public', async () => {
-    mockReflector.getAllAndOverride.mockReturnValue(true);
+  describe('should throw UnauthorizedException', () => {
+    beforeEach(() => {
+      mockReflector.getAllAndOverride.mockReturnValue(false);
+    });
 
-    const ctx = makeContext();
-    const result = await guard.canActivate(ctx);
-    expect(result).toBe(true);
-    expectJwtNotCalled();
+    it('when token is missing', async () => {
+      const ctx = makeAuthContext();
+
+      const actual = guard.canActivate(ctx);
+
+      await expect(actual).rejects.toThrow(UnauthorizedException);
+      expect(mockJwtService.verifyAsync).not.toHaveBeenCalled();
+    });
+
+    it('when token is invalid', async () => {
+      mockJwtService.verifyAsync.mockRejectedValue(new Error('invalid'));
+      const ctx = makeAuthContext(INVALID_TOKEN);
+
+      const actual = guard.canActivate(ctx);
+
+      await expect(actual).rejects.toThrow(UnauthorizedException);
+      expect(mockJwtService.verifyAsync).toHaveBeenCalledWith(
+        INVALID_TOKEN,
+        expect.objectContaining({ secret: expect.any(String) }),
+      );
+    });
   });
 
-  it('should throw UnauthorizedException if token is missing', async () => {
-    mockReflector.getAllAndOverride.mockReturnValue(false);
+  describe('should allow access ', () => {
+    it('when route is public', async () => {
+      mockReflector.getAllAndOverride.mockReturnValue(true);
+      const ctx = makeContext();
 
-    const ctx = makeAuthContext();
-    await expect(guard.canActivate(ctx)).rejects.toThrow(UnauthorizedException);
-    expectJwtNotCalled();
-  });
+      const actual = await guard.canActivate(ctx);
 
-  it('should throw UnauthorizedException if token is invalid', async () => {
-    mockReflector.getAllAndOverride.mockReturnValue(false);
-    mockJwtService.verifyAsync.mockRejectedValue(new Error('invalid'));
+      expect(actual).toBeTruthy();
+      expect(mockJwtService.verifyAsync).not.toHaveBeenCalled();
+    });
 
-    const ctx = makeAuthContext(INVALID_TOKEN);
-    await expect(guard.canActivate(ctx)).rejects.toThrow(UnauthorizedException);
-    expect(mockJwtService.verifyAsync).toHaveBeenCalledWith(
-      INVALID_TOKEN,
-      expect.objectContaining({ secret: expect.any(String) }),
-    );
-  });
+    it('when token is valid and attach user to request', async () => {
+      mockReflector.getAllAndOverride.mockReturnValue(false);
+      mockJwtService.verifyAsync.mockResolvedValue(MOCK_USER);
+      const ctx = makeAuthContext(VALID_TOKEN);
 
-  it('should allow access if token is valid and attach user to request', async () => {
-    const mockUser = { id: faker.database.mongodbObjectId() };
-    mockReflector.getAllAndOverride.mockReturnValue(false);
-    mockJwtService.verifyAsync.mockResolvedValue(mockUser);
+      const actual = await guard.canActivate(ctx);
 
-    const ctx = makeAuthContext(VALID_TOKEN);
-    const result = await guard.canActivate(ctx);
-    expect(result).toBe(true);
-
-    expect(mockJwtService.verifyAsync).toHaveBeenCalledWith(
-      VALID_TOKEN,
-      expect.objectContaining({ secret: expect.any(String) }),
-    );
-    const req = ctx.switchToHttp().getRequest();
-    expect(req.user).toEqual(mockUser);
+      expect(actual).toBeTruthy();
+      expect(mockJwtService.verifyAsync).toHaveBeenCalledWith(
+        VALID_TOKEN,
+        expect.objectContaining({ secret: expect.any(String) }),
+      );
+      const req = ctx.switchToHttp().getRequest();
+      expect(req.user).toEqual(MOCK_USER);
+    });
   });
 });
