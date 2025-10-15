db1996f99f0a7a3da47c02dc9e2f377897532146
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2025-10-15 18:38:40 +0300

    refactor(jest-setup): refactor jest.setup.ts

--- server/jest.setup.ts
+++ server/jest.setup.ts
@@ -1,31 +1,24 @@
 import { ConfigService } from '@nestjs/config';
 
+const mockEnvs: Record<string, string> = {
+  S3_REGION: 'test-region',
+  S3_BUCKET_ID: 'test-bucket',
+  SPACES_KEY: 'test-key',
+  SPACES_SECRET: 'test-secret',
+  FILE_STORAGE_FOLDER: 'TEST',
+};
+
 beforeAll(() => {
-  jest.spyOn(ConfigService.prototype, 'get').mockImplementation((key: string) => {
-    const mockEnvs: Record<string, string> = {
-      S3_REGION: 'test-region',
-      S3_BUCKET_ID: 'test-bucket',
-      SPACES_KEY: 'test-key',
-      SPACES_SECRET: 'test-secret',
-      FILE_STORAGE_FOLDER: 'TEST',
-    };
-    
-    return mockEnvs[key] ?? null;
-  });
-
-  jest.spyOn(ConfigService.prototype, 'getOrThrow').mockImplementation((key: string) => {
-    const mockEnvs: Record<string, string> = {
-      S3_REGION: 'test-region',
-      S3_BUCKET_ID: 'test-bucket',
-      SPACES_KEY: 'test-key',
-      SPACES_SECRET: 'test-secret',
-      FILE_STORAGE_FOLDER: 'TEST',
-    };
-
+  const getMock = (key: string) => mockEnvs[key] ?? null;
+  
+  const getOrThrowMock = (key: string) => {
     const value = mockEnvs[key];
 
     if (!value) throw new Error(`Missing config for key: ${key}`);
 
     return value;
-  });
+  };
+
+  jest.spyOn(ConfigService.prototype, 'get').mockImplementation(getMock);
+  jest.spyOn(ConfigService.prototype, 'getOrThrow').mockImplementation(getOrThrowMock);
 });
